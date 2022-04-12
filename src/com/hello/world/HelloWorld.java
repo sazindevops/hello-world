6466c275de69173b8212957ff4edf6c8b6c7c5d6
Sazin Rahim <sazindevops@gmail.com>
2022-04-11 23:31:23 -0500

    this is java world project commit

---
+++ src/com/hello/world/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.hello.world;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println(" Welcome to Java World, I am learning Java ");
+    }
+
+}
